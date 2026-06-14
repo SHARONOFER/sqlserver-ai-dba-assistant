cad63ff1fb17fa592d814c35cc2e336686c20d62
sharon <sharonofer@gmail.com>
2026-06-14 15:29:59 +0300

    modified:   app/run_dba_assistant.py

--- app/run_dba_assistant.py
+++ app/run_dba_assistant.py
@@ -1,17 +1,19 @@
-from agents.dba_agent import generate_mock_dba_answer
+from agents.dba_agent import generate_real_dba_answer
 
 
 def main():
     print("SQL Server AI DBA Assistant")
     print("---------------------------")
 
-    user_question = input("Enter your DBA question: ")
+    user_question = input("User question:\n")
 
-    answer = generate_mock_dba_answer(
+    answer = generate_real_dba_answer(
         user_question=user_question,
         top_n=1
     )
 
+    print("\nAI DBA Answer:")
+    print("--------------")
     print(answer)
 
 
